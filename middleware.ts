21a83a2d60b6b7cf2ee4e3d1ecf784d541642eee
Austin Buckler <austin@lionhat.co>
2023-10-03 09:35:47 -0600

    first commit.

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { rewrite, next } from "@vercel/edge";
+
+export const config = {
+  matcher: "/",
+};
+
+export default function middleware(req: Request) {
+  const url = new URL(req.url);
+  const hostname = url.hostname;
+
+  if (hostname.startsWith("suits")) {
+    return rewrite(new URL(`/suits${url.pathname}`, req.url));
+  }
+
+  return next();
+}
